193d0c748d1fe968bbfdb9ac768b5fdb1dcfaa84
GuyPie <perkal@gmail.com>
2015-03-24 19:33:15 +0200

    Update facebookConnectPlugin.js
    fixed error when running ionic

--- facebookConnectPlugin.js
+++ facebookConnectPlugin.js
@@ -9,7 +9,7 @@
  *
  */
 
-if (cordova.platformId == "browser") {
+if (!window.cordova || cordova.platformId == "browser") {
 
     var facebookConnectPlugin = {
 
@@ -173,57 +173,4 @@ if (cordova.platformId == "browser") {
             }
         }
     }());
-
-    module.exports = facebookConnectPlugin;
-
-} else {
-
-    var exec = require("cordova/exec");
-
-    var facebookConnectPlugin = {
-
-        getLoginStatus: function (s, f) {
-            exec(s, f, "FacebookConnectPlugin", "getLoginStatus", []);
-        },
-
-        showDialog: function (options, s, f) {
-            exec(s, f, "FacebookConnectPlugin", "showDialog", [options]);
-        },
-
-        login: function (permissions, s, f) {
-            exec(s, f, "FacebookConnectPlugin", "login", permissions);
-        },
-
-        logEvent: function(name, params, valueToSum, s, f) {
-            // Prevent NSNulls getting into iOS, messes up our [command.argument count]
-            if (!params && !valueToSum) {
-                exec(s, f, "FacebookConnectPlugin", "logEvent", [name]);
-            } else if (params && !valueToSum) {
-                exec(s, f, "FacebookConnectPlugin", "logEvent", [name, params]);
-            } else if (params && valueToSum) {
-                exec(s, f, "FacebookConnectPlugin", "logEvent", [name, params, valueToSum]);
-            } else {
-                f("Invalid arguments");
-            }
-        },
-
-        logPurchase: function(value, currency, s, f) {
-            exec(s, f, "FacebookConnectPlugin", "logPurchase", [value, currency]);
-        },
-
-        getAccessToken: function(s, f) {
-            exec(s, f, "FacebookConnectPlugin", "getAccessToken", []);
-        },
-
-        logout: function (s, f) {
-            exec(s, f, "FacebookConnectPlugin", "logout", []);
-        },
-
-        api: function (graphPath, permissions, s, f) {
-            if (!permissions) { permissions = []; }
-            exec(s, f, "FacebookConnectPlugin", "graphApi", [graphPath, permissions]);
-        }
-    };
-
-    module.exports = facebookConnectPlugin;
 }
